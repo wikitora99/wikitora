bec4dfb5de9ee21810547e7cfde5ce52fa901f96
wikitora99 <wiki.tora99@gmail.com>
2022-01-31 20:19:39 +0700

    updated several files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" >
-  <title>Wikitora - Personal Blog</title>
+  <title>Wikitora - A Living Robot</title>
   <link rel="stylesheet" type="text/css" href="css/style.css">
-  <link rel="icon" href="img/icons/brandr.png">
+  <link rel="icon" href="img/favicon.ico">
 </head>
 <body>
   <ul>
