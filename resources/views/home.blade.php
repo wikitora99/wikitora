0fb52ae11c806a7daa7adf99b4026fbfcf876dfe
wikitora99 <wiki.tora99@gmail.com>
2022-01-31 17:50:05 +0700

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+  <title>Wikitora - Personal Blog</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="icon" href="img/icons/brandr.png">
+</head>
+<body>
+  <ul>
+    <li>{{ $name }}</li>
+    <li>{{ $mail }}</li>
+    <li>{{ $role }}</li>
+  </ul>
+  <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
