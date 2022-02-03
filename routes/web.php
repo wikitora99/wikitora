2e4b75d1e5a07b3d31df56fa5c5d096b7bf262bd
wikitora99 <wiki.tora99@gmail.com>
2022-02-03 18:43:37 +0700

    view: modified template

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+// Route::get('/', function () {
+//   return view('welcome');
+// });
+
+
 Route::get('/', function () {
-  return view('welcome');
+  return view('pages.home');
 });
-
-
 
